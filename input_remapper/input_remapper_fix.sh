22201fb03d93e8bf21e699a490471b8d2c00fcc7
zeosofyork <zeosofyork@gmail.com>
2024-01-26 16:20:15 -0500

    added apps and reconfigured flow

--- input_remapper/input_remapper_fix.sh
+++ input_remapper/input_remapper_fix.sh
@@ -4,5 +4,6 @@
 #https://github.com/boltgolt/howdy/issues/858
 #Install necessary python packages
 sudo apt install libdbus-1-dev python3-evdev python3-pydbus libgtksourceview-4-dev python3-pydantic
-
+cd /etc/howdy/dlib-data
+sudo ./install.sh
 sudo dpkg -i ./*.deb
--- zeusofyork_universal_tool.py
+++ zeusofyork_universal_tool.py
@@ -3,28 +3,35 @@ import os
 import subprocess
 
 
-'''
-def decompress_xz_file(kernel_path, output_folder):
-    # Make sure the output folder exists
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    # Extract the file name (without extension) from the xz path
-    file_name = os.path.basename(kernel_path).replace('.xz', '')
-    
-    # Full path for the decompressed file
-    decompressed_file_path = os.path.join(output_folder, file_name)
-
-    with lzma.open(kernel_path, 'rb') as f:
-        with open(decompressed_file_path, 'wb') as decompressed_file:
-            decompressed_file.write(f.read())
-'''
-
-
-
 menu = input("""Please select from the following options: 
     1. Linux
     2. Windows
     3. Android""" + '\n')
+
+
+
+
+
+def send_command(linux_apps,command):
+ 
+    sent_command = str(command)
+    result1 = subprocess.run(sent_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    # Get the output and error (if any)
+    output1 = result1.stdout
+    error1 = result1.stderr
+            # Check if the command was successful
+    if result1.returncode == 0:
+        print("Command executed successfully!")
+        print("Output:\n", output1)
+    else:
+        print("Command failed with return code:", result1.returncode)
+        if error1:
+            print("Error:\n", error1)
+        else:
+            print('test')
+
+
 
 
 if int(menu) == 1:
@@ -33,67 +40,89 @@ if int(menu) == 1:
         2. Kernel Builder
         """)
     if int(linux_tools) == 1:
-        linux_tools_submenu = input("""Which tool?: 
-        1. Kernel Builder
-        2. Howdy reinstallation
-        """ + '\n')
-        if int(linux_tools_submenu) == 2:
-            howdy_install = """cd ./tmp && sudo ../howdy/fix_howdy.sh && sudo rm -f ./*"""
-            result1 = subprocess.run(howdy_install, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        linux_tools_submenu = input("""Please make a selection: 
+            1. Applications
+            2. Linux Apps/packages
+            """ + '\n')
+        if int(linux_tools_submenu) == 1:
+            linux_apps = input("""Please make a selection: 
+            1. Input Remapper
+            2. Discord
+            3. Plex
+            """ + '\n')
+            if int(linux_apps) == 1:
+                #add installer for input remapper
+                command = "sudo ./input_remapper/input_remapper_fix.sh"
+                send_command(linux_apps,command)
 
-            # Get the output and error (if any)
-            output1 = result1.stdout
-            error1 = result1.stderr
-                    # Check if the command was successful
-            if result1.returncode == 0:
-                print("Command executed successfully!")
-                print("Output:\n", output1)
-            else:
-                print("Command failed with return code:", result1.returncode)
-                if error1:
-                    print("Error:\n", error1)
+                print('test')
+            elif int(linux_apps) == 2:
+                command = "sudo snap install discord"
+                send_command(linux_apps,command)
+                
+                linux_apps = input("""Please make a selection: 
+                1. Input Remapper
+                2. Discord
+                3. Plex
+                """ + '\n')
+            
+            
+        else:
+            linux_tools_submenu = input("""Please make a selection: 
+            1. Howdy reinstallation
+
+            """ + '\n')
+
+            if int(linux_tools_submenu) == 1:    
+                howdy_install = """cd ./tmp && sudo ../howdy/fix_howdy.sh && sudo rm -f ./*"""
+                result1 = subprocess.run(howdy_install, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+                # Get the output and error (if any)
+                output1 = result1.stdout
+                error1 = result1.stderr
+                        # Check if the command was successful
+                if result1.returncode == 0:
+                    print("Command executed successfully!")
+                    print("Output:\n", output1)
                 else:
-                    print('test')
+                    print("Command failed with return code:", result1.returncode)
+                    if error1:
+                        print("Error:\n", error1)
+                    else:
+                        print('test')
                     
-                    
-                    
-    elif int(linux_tools) == 2:
-        kernel_path = str(input("""Please give the path to the kernel zip: """))
-        extract_dir = str(os.getcwd())
-        #command = "./build_kernel.sh"
-        command1 = """tar -xvf {} ; extracted_folder_name=$(basename "${}") ; sudo cp /boot/config-`uname -r` ./$extracted_folder_name/.config  """.format(kernel_path, kernel_path,'linux-')
-        #command2 = "extracted_folder_name=$(basename "${}" .tar.xz && sudo cp /boot/config-`uname -r` ./$extracted_folder_name\.config".format(kernel_path)
         
-        result1 = subprocess.run(command1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                    
+else:
+    kernel_path = str(input("""Please give the path to the kernel zip: """))
+    extract_dir = str(os.getcwd())
+    #command = "./build_kernel.sh"
+    command1 = """tar -xvf {} ; extracted_folder_name=$(basename "${}") ; sudo cp /boot/config-`uname -r` ./$extracted_folder_name/.config  """.format(kernel_path, kernel_path,'linux-')
+    #command2 = "extracted_folder_name=$(basename "${}" .tar.xz && sudo cp /boot/config-`uname -r` ./$extracted_folder_name\.config".format(kernel_path)
+    
+    result1 = subprocess.run(command1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-        # Get the output and error (if any)
-        output1 = result1.stdout
-        error1 = result1.stderr
+    # Get the output and error (if any)
+    output1 = result1.stdout
+    error1 = result1.stderr
 
 
-        # Check if the command was successful
-        if result1.returncode == 0:
-            print("Command executed successfully!")
-            print("Output:\n", output1)
-        else:
-            print("Command failed with return code:", result1.returncode)
-            if error1:
-                print("Error:\n", error1)
-        """        
-        if result2.returncode == 0:
-            print("Command executed successfully!")
-            print("Output:\n", output2)
-        else:
-            print("Command failed with return code:", result2.returncode)
-            if error:
-                print("Error:\n", error2)
-        """
+    # Check if the command was successful
+    if result1.returncode == 0:
+        print("Command executed successfully!")
+        print("Output:\n", output1)
+    else:
+        print("Command failed with return code:", result1.returncode)
+        if error1:
+            print("Error:\n", error1)
+    """        
+    if result2.returncode == 0:
+        print("Command executed successfully!")
+        print("Output:\n", output2)
+    else:
+        print("Command failed with return code:", result2.returncode)
+        if error:
+            print("Error:\n", error2)
+    """
                 
         
-
-    else:
-        print('test')
-
-else:
-    print("goodbye")
-
